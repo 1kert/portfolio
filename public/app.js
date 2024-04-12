e246e5b9ce06bdee445ca73f5dbe1a05d7c30c6e
Kert Mänd <kertmand1@gmail.com>
2024-04-12 16:02:47 +0300

    left main animations

--- public/app.js
+++ public/app.js
@@ -1,10 +1,17 @@
-
-
-let docs = document.querySelectorAll(".fadein-right");
+let leftMain = document.querySelectorAll(".fadein-left");
 setTimeout(() => {
-    for(let i = 0; i < docs.length; i++){
+    for(let i = 0; i < leftMain.length; i++){
         setTimeout(() => {
-            docs[i].classList.remove("fadein-right");
-        }, i * 200);
+            leftMain[i].classList.remove("fadein-left");
+        }, i * 100);
     }
 }, 100);
+
+let rightMain = document.querySelectorAll(".fadein-right");
+setTimeout(() => {
+    for(let i = 0; i < rightMain.length; i++){
+        setTimeout(() => {
+            rightMain[i].classList.remove("fadein-right");
+        }, i * 100);
+    }
+}, 400);
